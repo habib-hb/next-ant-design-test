4a9c9b4370258efff7cb6b772adfcfe709817ee5
habib-hb <developerhabib1230@gmail.com>
2024-12-25 18:20:35 +0600

    nice

--- src/app/ant-design/page.tsx
+++ src/app/ant-design/page.tsx
@@ -15,6 +15,11 @@ import type { CalendarProps } from 'antd';
 import type { BadgeProps } from 'antd';
 import { Badge } from 'antd';
 import { BarChart } from '@mui/x-charts/BarChart';
+import Stack from '@mui/material/Stack';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { mangoFusionPalette } from '@mui/x-charts/colorPalettes';
 import {
   AutoComplete,
   Cascader,
@@ -51,6 +56,22 @@ const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
   console.log(value.format('YYYY-MM-DD'), mode);
 };
 
+
+
+const defaultSeries = [
+  { id: '1', data: [4, 5, 1, 2, 3, 3, 2], area: true, stack: '1' },
+  { id: '2', data: [7, 4, 6, 7, 2, 3, 5], area: true, stack: '1' },
+  { id: '3', data: [6, 4, 1, 2, 6, 3, 3], area: true, stack: '1' },
+  { id: '4', data: [4, 7, 6, 1, 2, 7, 7], area: true, stack: '1' },
+  { id: '5', data: [2, 2, 1, 7, 1, 5, 3], area: true, stack: '1' },
+  { id: '6', data: [6, 6, 1, 6, 7, 1, 1], area: true, stack: '1' },
+  { id: '7', data: [7, 6, 1, 6, 4, 4, 6], area: true, stack: '1' },
+  { id: '8', data: [4, 3, 1, 6, 6, 3, 5], area: true, stack: '1' },
+  { id: '9', data: [7, 6, 2, 7, 4, 2, 7], area: true, stack: '1' },
+].map((item, index) => ({
+  ...item,
+  color: mangoFusionPalette('light')[index],
+}));
 
 
 const justifyOptions = [
@@ -141,6 +162,11 @@ const { TreeNode } = TreeSelect;
 
 
  export default function AntDesign() {
+
+
+  const [series, setSeries] = React.useState(defaultSeries);
+  const [nbSeries, setNbSeries] = React.useState(3);
+  const [skipAnimation, setSkipAnimation] = React.useState(false);
 
   const [justify, setJustify] = useState<FlexProps['justify']>(justifyOptions[0]);
   const [alignItems, setAlignItems] = useState<FlexProps['align']>(alignOptions[0]);
@@ -506,6 +532,69 @@ const { TreeNode } = TreeSelect;
               />
         </div>
 
+
+
+            <br /><br /><br /><br />
+
+
+
+   <div
+      style={{
+        width: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 'auto',
+      }}
+    >
+      <div>
+        <LineChart
+          xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7] }]}
+          series={[
+            ...series.slice(0, Math.min(nbSeries, 8)),
+            ...series.slice(8, 10),
+          ]}
+          skipAnimation={skipAnimation}
+          height={400}
+        />
+      </div>
+      <Stack spacing={1} direction="row">
+        <Button
+          variant="outlined"
+          onClick={() =>
+            setSeries((prev) =>
+              prev.map((item) => ({
+                ...item,
+                data: item.data.map((v) => Math.max(0.5, v - 4 + 8 * Math.random())),
+              })),
+            )
+          }
+        >
+          randomize
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => setNbSeries((prev) => prev - 1)}
+          disabled={nbSeries === 0}
+        >
+          remove
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => setNbSeries((prev) => prev + 1)}
+          disabled={nbSeries === 8}
+        >
+          add
+        </Button>
+        <FormControlLabel
+          checked={skipAnimation}
+          control={
+            <Checkbox onChange={(event) => setSkipAnimation(event.target.checked)} />
+          }
+          label="skipAnimation"
+          labelPlacement="end"
+        />
+      </Stack>
+    </div>
 
         
      </div>
